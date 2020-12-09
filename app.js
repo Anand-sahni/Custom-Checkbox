dc68307b95bfc9c587b98d14f5db2cacbec69f2c
Anand Sahni <75078213+Anand-sahni@users.noreply.github.com>
2020-12-09 10:05:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let checkbox = document.getElementById("cb");
+let svg = document.getElementById("checkbox");
+
+svg.addEventListener("click", () => {
+  if (checkbox.checked) svg.classList.add("reverse");
+  else svg.classList.remove("reverse");
+});
